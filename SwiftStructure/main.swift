89933db02facdbf271a28caacc54e5b516d4c61f
Ahmed Onawale <ahmed.onawale@andela.com>
2016-03-09 00:43:51 +0100

    Initial Commit

---
+++ SwiftStructure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftStructure
+//
+//  Created by Ahmed Onawale on 3/9/16.
+//  Copyright © 2016 Ahmed Onawale. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
